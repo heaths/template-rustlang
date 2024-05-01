2638948a13259d2b32303281998b7097081fe976
Heath Stewart <heaths@microsoft.com>
2024-05-01 12:50:23 -0700

    Add error template

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 // Copyright {{param "copyright"}} Heath Stewart.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+{{if (param "error" true "Do you want an error template?") -}}
+mod error;
+pub use error::*;
+{{- else -}}
+{{deleteFile "src/error.rs"}}
+{{- end }}
+
 pub fn say_hello(name: Option<&str>) -> String {
     format!("Hello, {}!", name.unwrap_or("world"))
 }
